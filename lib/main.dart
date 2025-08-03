2783ae6bd43e24b41091aa2597ccfc35c58b5e0c
Marianna <mariannabosiaka@gmail.com>
2025-08-04 00:39:29 +0300

    removed status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:focus_timer_app/app.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Full immersive mode: hides status and nav bars (including icons)
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
   runApp(const MyApp());
 }
-
